11f6994fa135e6bc6814a75036d12a9b216042c7
jefferson <devops.jeffersantos@gmail.com>
2024-03-17 22:22:19 -0300

    change pom.xml

---
+++ tech_challenge_03/findFoodApi/findfoodback/src/test/java/com/techchallenge/restaurant/api/findfood/service/ReservaServiceIntegrationTest.java
@@ -0,0 +1,160 @@
+package com.techchallenge.restaurant.api.findfood.service;
+
+import com.techchallenge.restaurant.api.findfood.api.model.ReservaDTO;
+import com.techchallenge.restaurant.api.findfood.dados.ReservaDados;
+import com.techchallenge.restaurant.api.findfood.domain.repository.ReservaRepository;
+import com.techchallenge.restaurant.api.findfood.domain.repository.RestauranteRepository;
+import com.techchallenge.restaurant.api.findfood.domain.service.ReservaService;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Sql(scripts = "/data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
+public class ReservaServiceIntegrationTest extends ReservaDados {
+
+    @Autowired
+    private ReservaRepository reservaRepository;
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    private ReservaService reservaService;
+
+    private ModelMapper modelMapper = new ModelMapper();
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+
+    }
+
+
+    @Test
+    void deveReservarComSucesso() {
+        var restaurante = criarRestauranteValido();
+        var reserva = criarReservaDto();
+
+        var restauranteSalvo = restauranteRepository.save(restaurante);
+
+        var reservaArmazenada = reservaService.reservarMesa(restauranteSalvo.getId(), reserva);
+
+        assertThat(reservaArmazenada)
+                .isNotNull()
+                .isInstanceOf(ReservaDTO.class);
+        assertThat(reservaArmazenada.getId())
+                .isNotNull();
+    }
+
+    @Test
+    void deveBuscarReservaPeloIdComSucesso() {
+        var restauranteId = 1L;
+        var reserva = criarReservaDto();
+
+        var reservaRealizada = reservaService.reservarMesa(restauranteId, reserva);
+
+        var buscarReserva = reservaService.findById(reservaRealizada.getId());
+
+
+        assertThat(buscarReserva)
+                .isNotNull()
+                .isInstanceOf(ReservaDTO.class);
+
+    }
+
+    @Test
+    void deveLancarExceptionAoBuscarReservaPeloIdNaoEncontrado() {
+        var restauranteId = 1L;
+        var reserva = criarReservaDto();
+        reserva.setId(0L);
+
+        var reservaRealizada = reservaService.reservarMesa(restauranteId, reserva);
+
+        assertThatThrownBy(() -> reservaService.findById(reserva.getId()))
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("Reserva não encontrada!");
+    }
+
+    @Test
+    void deveLancarExceptionAoTentarReservarMesaComRestauranteNaoEncontrado() {
+        var restauranteId = 0L;
+        var reservaDTO = criarReservaDto();
+
+        assertThatThrownBy(() -> reservaService.reservarMesa(restauranteId, reservaDTO))
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("Restaurante não existe");
+    }
+
+    @Test
+    void deveLancarExceptionAoTentarReservarMesaComRestauranteSemLugaresDisponiveis() {
+        var restauranteId = 1L;
+        var reservaDTO = criarReservaDto();
+        reservaDTO.setQtdPessoas(4000);
+
+
+
+            assertThatThrownBy(() -> reservaService.reservarMesa(restauranteId, reservaDTO))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("Não há lugares disponíveis nesse horário para o Restaurante: Restaurante Teste");
+
+
+
+    }
+
+    @Test
+    void deveLancarExceptionAoTentarReservarMesaComRestauranteSemHorarioDisponivel() {
+        var restauranteId = 1L;
+        var reservaDTO = criarReservaDto();
+        reservaDTO.setQtdPessoas(200);
+        reservaDTO.setDataHoraInicio(LocalDateTime.parse("2024-03-27T10:15:30"));
+        reservaDTO.setDataHoraFim(LocalDateTime.parse("2024-03-27T12:15:30"));
+
+
+            //assertThrows(IllegalArgumentException.class, () -> reservaService.reservarMesa(restauranteId, reservaDTO));
+            assertThatThrownBy(() -> reservaService.reservarMesa(restauranteId, reservaDTO))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("Não há lugares disponíveis nesse horário para o Restaurante: Restaurante Teste");
+
+
+
+
+    }
+
+    @Test
+    @Order(2)
+    void deveRetornarTodasAsReservasComSucesso() {
+        var restauranteId = 1L;
+        var reserva = criarReservaDto();
+
+        var reservaRealizada = reservaService.reservarMesa(restauranteId, reserva);
+
+        var result = reservaService.findAll(restauranteId);
+
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    void deveLancarExceptionAoTentarReservarMesaEmRestauranteQueNaoExiste() {
+        var restauranteId = 0L;
+
+        assertThatThrownBy(() -> reservaService.findAll(restauranteId))
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("Restaurante não existe");
+
+    }
+
+}
